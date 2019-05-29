5ec5a793c4799eb4cbddb4e6dea06d38da34e26b
roccosi <mibcreative@gmail.com>
2019-05-29 16:04:11 +0200

    Final with Splash

--- lib/main.dart
+++ lib/main.dart
@@ -7,8 +7,14 @@ void main() {
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
-          title: Text('Dicee'),
-          backgroundColor: Colors.red,
+          title: Text(
+            'Dicee',
+            style: TextStyle(
+                color: Colors.red,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 2.0),
+          ),
+          backgroundColor: Colors.white,
         ),
         body: DicePage(),
       ),
@@ -23,33 +29,68 @@ class DicePage extends StatefulWidget {
 
 class _DicePageState extends State<DicePage> {
   int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
+  void rollTheDice() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
+
+    print('$leftDiceNumber & $rightDiceNumber');
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Row(
-        children: <Widget>[
-          Expanded(
-            child: FlatButton(
-              onPressed: () {
-                setState(() {
-                  leftDiceNumber = 5;
-                });
-                print('Left button got pressed');
-              },
-              child: Image.asset('images/dice$leftDiceNumber.png'),
+    return InkWell(
+      highlightColor: Colors.white,
+      splashColor: Colors.transparent,
+      onTap: () {
+        setState(() {
+          rollTheDice();
+        });
+      },
+      child: Center(
+        child: Row(
+          children: <Widget>[
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Image.asset('images/dice$leftDiceNumber.png'),
+              ),
             ),
-          ),
-          Expanded(
-            child: FlatButton(
-              onPressed: () {
-                print('Right button got pressed');
-              },
-              child: Image.asset('images/dice1.png'),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Image.asset('images/dice$rightDiceNumber.png'),
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
 }
+
+//@override
+//Widget build(BuildContext context) {
+//  return Center(
+//    child: Row(
+//      children: <Widget>[
+//        Expanded(
+//          child: Padding(
+//            padding: const EdgeInsets.all(16.0),
+//            child: Image.asset('images/dice$leftDiceNumber.png'),
+//          ),
+//        ),
+//        Expanded(
+//          child: Padding(
+//            padding: const EdgeInsets.all(16.0),
+//            child: Image.asset('images/dice$rightDiceNumber.png'),
+//          ),
+//        ),
+//      ],
+//    ),
+//  );
+//}
+//}
